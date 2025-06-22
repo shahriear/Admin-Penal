be511a99a531502b0e6d76938f438ad09951dde8
shuvo pc <shahriearshuvo@gmail.com>
2025-06-22 10:47:15 +0600

    gg

--- src/components/RecentActivities.jsx
+++ src/components/RecentActivities.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 const RecentActivities = () => {
   return (
-    <div className="bg-white p-4 rounded">
-      <h4 className="font-bold mb-4 text-[12px] md:text-base">
+    <div className="bg-white">
+      <h4 className="font-bold py-5 mb-2  bg-gray-100 text-[12px] md:text-base">
         Recent Activities
       </h4>
 
       <div className="space-y-2">
         {/* Row 1 */}
-        <div className="bg-gray-100 px-2 py-1 rounded grid grid-cols-4 text-[10px] items-center">
-          <div>22.09.2023</div>
+        <div className="bg-gray-100 px-2 py-1 text-center rounded grid grid-cols-4 text-[10px] items-center">
+          <div className="text-start">22.09.2023</div>
           <div>TXDFGC2ORRN</div>
           <div>$120000</div>
-          <div className="text-green-600 font-semibold text-right">Done</div>
+          <div className="text-green-600 font-semibold ">Done</div>
         </div>
 
         {/* Row 2 */}
